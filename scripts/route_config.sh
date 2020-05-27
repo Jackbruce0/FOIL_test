699c3542e60a448a3734495b239d7e1fb3a1107c
Jack Bruce <jackbruce0@gmail.com>
2020-05-27 16:10:02 -0700

    Permanent routing accomplished
    see config_file/config_notes.txt for details

--- scripts/route_config.sh
+++ scripts/route_config.sh
@@ -3,6 +3,8 @@
 # RUN THIS SCRIPT AS ROOT 
 
 # this script will force loopback traffic through ethernet ports (given ip's arent changed)
+# not necessary anymore with all of the system configurations (see config-files/)
+
 # enp1s0 = 10.50.0.1
 # enp3s0 = 10.50.1.1
 ip_1="10.50.0.1"
@@ -13,22 +15,22 @@ iface_1="enp1s0"
 iface_2="enp3s0"
 
 # set static ip's for interfaces (just incase they got messed up)
-#ip addr add $ip_1 dev $iface_1 
-#ip addr add $ip_2 dev $iface_2 
+ip addr add $ip_1 dev $iface_1 
+ip addr add $ip_2 dev $iface_2 
 # ABOVE STEPS CURRENTLY ACCOMPLISHED WITH NETPLAN
 
 
 # nat source IP 10.50.0.1 -> 10.60.0.1 when going to 10.60.1.1
-#iptables -t nat -A POSTROUTING -s $ip_1 -d 10.60.1.1 -j SNAT --to-source 10.60.0.1
+iptables -t nat -A POSTROUTING -s $ip_1 -d 10.60.1.1 -j SNAT --to-source 10.60.0.1
 
 # nat inbound 10.60.0.1 -> 10.50.0.1
-#iptables -t nat -A PREROUTING -d 10.60.0.1 -j DNAT --to-destination $ip_1 
+iptables -t nat -A PREROUTING -d 10.60.0.1 -j DNAT --to-destination $ip_1 
 
 # nat source IP 10.50.1.1 -> 10.60.1.1 when going to 10.60.0.1
-#iptables -t nat -A POSTROUTING -s $ip_2 -d 10.60.0.1 -j SNAT --to-source 10.60.1.1
+iptables -t nat -A POSTROUTING -s $ip_2 -d 10.60.0.1 -j SNAT --to-source 10.60.1.1
 
 # nat inbound 10.60.1.1 -> 10.50.1.1
-#iptables -t nat -A PREROUTING -d 10.60.1.1 -j DNAT --to-destination $ip_2 
+iptables -t nat -A PREROUTING -d 10.60.1.1 -j DNAT --to-destination $ip_2 
 
 
 # Now tell the system how to get to each fake network, and prepopulate the arp entries
